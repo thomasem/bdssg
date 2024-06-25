d0f3a00b5cbdb674de1e8ddb4bf2b53cd1d390d5
thomasem <me@thomasem.net>
2024-06-25 12:56:12 -0500

    move to convention 'x is None' for None checks

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -26,11 +26,11 @@ class HTMLNode:
 
 class LeafNode(HTMLNode):
     def __init__ (self, tag=None, value=None, props=None):
-        if not value:
+        if value is None:
             raise ValueError("value required for LeafNode")
         super().__init__(tag=tag, value=value, props=props)
 
     def to_html(self):
-        if not self.tag:
+        if self.tag is None:
             return self.value
         return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
