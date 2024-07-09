5892f921441e784cd18a6da5b3b6b54e328826c1
thomasem <me@thomasem.net>
2024-07-09 10:23:45 -0500

    implement initial block_to_block_type with tests

--- src/parse.py
+++ src/parse.py
@@ -1,11 +1,21 @@
+from enum import Enum
 import re
 
 from textnode import TextNode, TextType
 
 
-# TODO: Consider Extract classes that encapsulates the logic for converting
-# to a TextNode, which would help handle cases where the extract isn't
-# two strings, like the tuple[str, str] from extracting an image or a link
+class BlockType(Enum):
+    Paragraph = "paragraph"
+    Heading = "heading"
+    Code = "code"
+    Quote = "quote"
+    UnorderedList = "unordered_list"
+    OrderedList = "ordered_list"
+
+
+# TODO: Consider Extract classes that encapsulate the logic for converting
+# to a TextNode, which would help handle cases where the extract isn't a
+# tuple[str, str]
 
 
 class Extractor:
@@ -133,3 +143,37 @@ def markdown_to_blocks(markdown: str) -> list[str]:
         if part != "":
             blocks.append(part.strip())
     return blocks
+
+
+def block_to_block_type(text: str) -> BlockType:
+    """This function will determine the type of markdown block that's passed
+    in, returning a BlockType. We can assume leading / trailing whitespace is
+    already removed from the markdown_to_blocks() call. Hope I don't regret the
+    regex. :)
+    """
+    # If it starts with 1-6 of the "#" character, we have a heading
+    if re.match(r'^#{1,6}\ ', text):
+        return BlockType.Heading
+    # If it starts and ends with three backtick (`) characters, we have a code
+    # block
+    if text.startswith("```") and text.endswith("```"):
+        return BlockType.Code
+
+    lines = text.splitlines()
+    flags = re.MULTILINE
+
+    # If all lines start with >, we've got a quote
+    if len(re.findall(r'^>.+$', text, flags=flags)) == len(lines):
+        return BlockType.Quote
+
+    # If all lines start with * or - and a space, we've got an unordered list
+    if len(re.findall(r'^[\*\-]\ .+$', text, flags=flags)) == len(lines):
+        return BlockType.UnorderedList
+
+    # If all lines start with "N." and a space, like "1. " or "2. "
+    # we have an ordered list as long as the first number is "1"
+    if text.startswith("1. ") and \
+            len(re.findall(r'^\d+\.\ .+$', text, flags=flags)) == len(lines):
+        return BlockType.OrderedList
+
+    return BlockType.Paragraph
--- src/test_parse.py
+++ src/test_parse.py
@@ -1,8 +1,10 @@
 import unittest
 
 from parse import (
+    BlockType,
     ImageExtractor,
     LinkExtractor,
+    block_to_block_type,
     markdown_to_blocks,
     split_nodes_delimiter,
     split_nodes_extractor,
@@ -382,6 +384,42 @@ This is the same paragraph on a new line
         ]
         results = markdown_to_blocks(markdown)
         self.assertEqual(expected, results)
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_block_to_block_type(self):
+        blocks = [
+            "* foo\n* bar",
+            "I'm just a little paragraph\nThere's nothing to see here!",
+            ">Ask not what you can do for your country!\n>Ask what's for lunch.",
+            "```\ndef main():\n    print('hello, world!')\n```",
+            "# Shopping list",
+            "###### Essentials",
+            "1. apples\n4. beer\n5. beer\n6. soft pretzels\n10. mustard",
+        ]
+        expected = [
+            BlockType.UnorderedList,
+            BlockType.Paragraph,
+            BlockType.Quote,
+            BlockType.Code,
+            BlockType.Heading,
+            BlockType.Heading,
+            BlockType.OrderedList
+        ]
+        self.assertEqual(len(blocks), len(expected))
+        for block, expected in zip(blocks, expected):
+            self.assertEqual(expected, block_to_block_type(block))
+
+    def test_block_to_block_type_negative(self):
+        blocks = [
+            "2. counting not starting at 1\n3. making sandwiches",
+            "####### I don't go to 7 levels",
+            "def ```main()``` is not a code block, but more inline",
+            "> almost a block quote\n but not really",
+            "* I was a list\n until I forgot what I was doing",
+        ]
+        for block in blocks:
+            self.assertEqual(BlockType.Paragraph, block_to_block_type(block))
 
 
 if __name__ == "__main__":
