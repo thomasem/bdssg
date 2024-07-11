965b8fba8f7cc23f0fcf28bd2c99e4dc452e716e
thomasem <me@thomasem.net>
2024-07-11 15:43:01 -0500

    implement recursive copy from static to public in main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,31 @@
-import textnode
+import shutil
+import os
+
+
+def copy(src: str, dest: str):
+    if os.path.exists(dest):
+        print(f"Found content at {dest}, removing...")
+        shutil.rmtree(dest)
+
+    print(f"Creating directory {dest}...")
+    os.mkdir(dest)
+
+    for basename in os.listdir(src):
+        path = os.path.join(src, basename)
+        if os.path.isdir(path):
+            next_dest = os.path.join(dest, basename)
+            print(f"Found directory {path}, copying into {next_dest}")
+            copy(path, next_dest)
+        else:
+            print(f"Copying {path} to {dest}")
+            shutil.copy(path, dest)
 
 
 def main():
-    text = ("This is **text** with an *italic* word and a `code block` and an "
-    "![image](https://image.hahaha) and a [link](https://link.url)")
-    for node in textnode.text_to_textnodes(text):
-        print(node)
+    public_path = os.path.abspath("public")
+    static_path = os.path.abspath("static")
+    print(f"Copying static files to {public_path}...")
+    copy(static_path, public_path)
 
 
 if __name__ == "__main__":
